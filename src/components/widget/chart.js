e71452a83e6803c45a0441e25339a723a7c64337
Sukruth M K <suk_mallur@yahoo.co.in>
2019-08-24 19:32:57 -0700

    added filter to charts

--- src/components/widget/chart.js
+++ src/components/widget/chart.js
@@ -4,15 +4,47 @@ import { Col, Form, Row } from "react-bootstrap";
 import { LineChart } from "react-chartkick";
 import "chart.js";
 
-import { RANGES } from "../../core/enum/dateRanges";
+import {
+  ONE_MONTH,
+  ONE_YEAR,
+  THREE_MONTH,
+  RANGES
+} from "../../core/enum/dateRanges";
 import { PadContainer } from "../../core/framework/layout";
 
 const Chart = ({ chartData, currency, range, setChartData, setRange }) => {
+  const formatDate = date => {
+    var d = new Date(date),
+      month = "" + (d.getMonth() + 1),
+      day = "" + d.getDate(),
+      year = d.getFullYear();
+
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
+
+    return [year, month, day].join("-");
+  };
+  const getUrl = React.useCallback(() => {
+    const date = new Date();
+    let startDate = "";
+    const endDate = formatDate(date);
+    if (range === ONE_MONTH) {
+      return `https://api.coindesk.com/v1/bpi/historical/close.json?currency=${currency}`;
+    }
+    if (range === THREE_MONTH) {
+      date.setMonth(date.getMonth() - 3);
+      startDate = formatDate(date);
+    }
+    if (range === ONE_YEAR) {
+      date.setMonth(date.getMonth() - 12);
+      startDate = formatDate(date);
+    }
+    return `https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}&currency=${currency}`;
+  }, [currency, range]);
+  const url = getUrl();
   React.useEffect(() => {
     async function fetchData() {
-      const response = await axios(
-        `https://api.coindesk.com/v1/bpi/historical/close.json?currency=${currency}`
-      );
+      const response = await axios(url);
       const { data } = response;
       if (data) {
         const { bpi } = data;
@@ -20,13 +52,13 @@ const Chart = ({ chartData, currency, range, setChartData, setRange }) => {
       }
     }
     fetchData();
-  }, [currency, range, setChartData]);
+  }, [currency, url, range, setChartData]);
   return (
     <div>
       <PadContainer>
         <Row>
           <Col>
-            <div class="float-right">
+            <div className="float-right">
               <Form>
                 <Form.Group controlId="exampleForm.ControlRange">
                   <Form.Control
@@ -34,10 +66,8 @@ const Chart = ({ chartData, currency, range, setChartData, setRange }) => {
                     value={range}
                     onChange={e => setRange(e.target.value)}
                   >
-                    {RANGES.map(range => (
-                      <>
-                        <option value={range}>{range}</option>
-                      </>
+                    {RANGES.map((range,index) => (
+                        <option value={range} key={index}>{range}</option>
                     ))}
                   </Form.Control>
                 </Form.Group>
