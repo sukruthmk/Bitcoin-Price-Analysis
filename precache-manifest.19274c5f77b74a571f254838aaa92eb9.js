459240647010564ca3abbe157c420d0ece795920
Sukruth M K <suk_mallur@yahoo.co.in>
2019-08-25 03:34:47 -0700

    Updates

---
+++ precache-manifest.19274c5f77b74a571f254838aaa92eb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5353d1bd00b882cb32a54540c4757b27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1266fd369378ec82765c",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "d584c2dfcb45308f2475",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1266fd369378ec82765c",
+    "url": "/static/js/2.8c8e2aed.chunk.js"
+  },
+  {
+    "revision": "d584c2dfcb45308f2475",
+    "url": "/static/js/main.fe5f0559.chunk.js"
+  },
+  {
+    "revision": "b4876879f67e4cd26991",
+    "url": "/static/js/runtime~main.a76049c8.js"
+  }
+]);
